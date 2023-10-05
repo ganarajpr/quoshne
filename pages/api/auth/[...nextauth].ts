b56be90137ff9737ae838fcddb253c538f12e132
Ganaraj Permunda <ganarajpr@gmail.com>
2023-10-05 10:33:20 +0100

    first commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { XataAdapter } from "@auth/xata-adapter"
+import { XataClient } from "../../../src/xata" // or wherever you've chosen to create the client
+
+const client = new XataClient()
+
+export default NextAuth({
+  adapter: XataAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+})
